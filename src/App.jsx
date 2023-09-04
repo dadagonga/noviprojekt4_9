11f67c8a6b8004f016a700e240b8b597c5d4687a
irena_weigand <irena.weigand@gmail.com>
2023-09-04 19:57:51 +0200

    add2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import cryptoRandomString from 'crypto-random-string'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,13 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Crypto</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Promjeni <code>src/App.jsx</code> i spremi
+          Random crypto kod  <code>cryptoRandomString({length: 10});</code> i spremi
         </p>
       </div>
       <p className="read-the-docs">
